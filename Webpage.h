73955ced86fdad59cc11fb75e0225a299f22d412
DianaJLeon <43466978+DianaJLeon@users.noreply.github.com>
2018-10-06 01:27:29 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -1,16 +1,50 @@
 #pragma once
+#ifndef webpage_h
+#define webpage_h
 
 #include <string>
 using namespace std;
 
 class Webpage {
 public:
-    Webpage();
-    Webpage(const string& webpageUrl, const time_t& timeVisited);
-    string getUrl();
-    time_t getTime();
+	Webpage();
+	Webpage(const string& webpageUrl, const time_t& timeVisited);
+	string getUrl();
+	time_t getTime();
 
 private:
-    string url;
-    time_t time;
+	string url;
+	time_t time;
 };
+// Default constructor 
+Webpage::Webpage()
+{
+
+
+
+}
+
+// 
+Webpage::Webpage(const string& webpageUrl, const time_t& timeVisited);
+{
+
+
+
+}
+
+// The getUrl() member function gets a URL from the text file
+string Webpage::getUrl()
+{
+
+
+}
+
+// 
+time_t Webpage::getTime()
+{
+
+
+}
+
+
+#endif 
