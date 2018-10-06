5a1251bed347b0408e189067ee37f3f8c688bf65
DianaJLeon <43466978+DianaJLeon@users.noreply.github.com>
2018-10-06 01:25:23 -0700

    Update BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -12,50 +12,97 @@ using namespace std;
 
 class BrowserHistory {
 public:
-    BrowserHistory();
+	BrowserHistory();
 
-    void visitSite(Webpage newSite);
-    string back();
-    string forward();
+	void visitSite(Webpage newSite);
+	string back();
+	string forward();
 
-    void readHistory(string fileName);
+	void readHistory(string fileName);
 
-    string getUrl();
-    size_t getNavSize();
-    list<Webpage> getSitesVisited();
+	string getUrl();
+	size_t getNavSize();
+	list<Webpage> getSitesVisited();
 
 private:
-    list<Webpage> navHistory;
-    list<Webpage>::iterator navPos;
-    list<Webpage> sitesVisited;
-    int numSites;
+	list<Webpage> navHistory;
+	list<Webpage>::iterator navPos;
+	list<Webpage> sitesVisited;
+	int numSites;
 };
 
 void BrowserHistory::readHistory(string fileName) {
-    string temp;
-    int newTime;
-    Webpage newSite;
-    ifstream inFile(fileName.c_str());
+	string temp;
+	int newTime;
+	Webpage newSite;
+	ifstream inFile(fileName.c_str());
 
-    while(inFile >> temp) {
-        if(temp == "new") {
-            inFile >> temp >> newTime;
-            newSite = Webpage(temp, time_t(newTime));
-            visitSite(newSite);
-        } else if(temp == "back") {
-            back();
-        } else if(temp == "forward") {
-            forward();
-        } else {
-            throw logic_error("invalid command");
-        }
-    }
+	while (inFile >> temp) {
+		if (temp == "new") {
+			inFile >> temp >> newTime;
+			newSite = Webpage(temp, time_t(newTime));
+			visitSite(newSite);
+		}
+		else if (temp == "back") {
+			back();
+		}
+		else if (temp == "forward") {
+			forward();
+		}
+		else {
+			throw logic_error("invalid command");
+		}
+	}
 }
 // Default Constructor
 BrowserHistory::BrowserHistory()
 {
-    
+
 }
 
+void BrowserHistory::visitSite(Webpage newsite)
+{
 
+}
+
+string BrowserHistory::back()
+{
+
+
+}
+
+//
+string BrowserHistory::forward()
+{
+
+
+}
+
+//
+void BrowserHistory::readHistory(string filename)
+{
+
+
+
+}
+
+//
+string BrowserHistory::getUrl()
+{
+
+
+}
+
+// The getNavSize() function 
+size_t BrowserHistory::getNavSize()
+{
+
+}
+
+// The getSitesVisited() function 
+list<Webpage> BrowserHistory::getSitesVisited()
+{
+
+
+}
 #endif
