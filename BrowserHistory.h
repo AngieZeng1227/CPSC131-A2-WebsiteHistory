9dd5857b20af616b9f55df532c5a59e7e090f4bb
ZZ1227 <43360802+ZZ1227@users.noreply.github.com>
2018-10-07 18:37:11 -0700

    Update BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -65,7 +65,7 @@ BrowserHistory::BrowserHistory()
 void BrowserHistory::visitSite(Webpage newSite)
 {
 	
-	while (navPos != (navHistory.end()--))
+	while (navPos != --navHistory.end())
 	{
 		if (numSites > 1)
 		{
